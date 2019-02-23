27eed79fa42ca522a8e5485670fdffc4062dbd78
郑珏 <3642214>
2019-02-23 21:37:44 +0800

    少提交文件

---
+++ Aria2-client-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aria2
+//
+//  Created by zj15 on 2018/3/27.
+//  Copyright © 2018年 zj15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
